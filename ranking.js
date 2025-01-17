224a4f88bac54153ffa62947e3686a198b705f4c
Romulo Lobato Bezerra <111526886+RomuloLB28@users.noreply.github.com>
2025-01-17 14:24:44 -0300

    Delete ranking.js

--- ranking.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const rankingTable = document.getElementById("rankingTable");
-    const ranking = JSON.parse(localStorage.getItem("ranking")) || [];
-  
-    rankingTable.innerHTML = ranking
-      .map((entry) => `<tr><td>${entry.name}</td><td>${entry.score}</td></tr>`)
-      .join("");
-  });
-  
